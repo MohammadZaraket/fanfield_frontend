bad434b6bedd19362e43140b68f513ebe35f4fcc
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-13 23:11:01 +0200

    adding comments how editing works

--- index.js
+++ index.js
@@ -38,13 +38,13 @@ window.onload = async (event) => {
     var edit_buttons = document.getElementsByClassName("edit");
     var delete_buttons = document.getElementsByClassName("delete");
 
-    console.log(posts.length);
+   /* console.log(posts.length);
     console.log(delete_buttons.length);
     console.log(edit_buttons.length);
-
+*/
     
     for (let i=0; i < delete_buttons.length; i++){
-        delete_buttons[i].onclick= async (event) => {
+        delete_buttons[i].onclick= async (event) => { // when delete button is clicked, call API to delete status and reload page
          
             let formData = new FormData();
             formData.append('post_id', delete_buttons[i].id);
@@ -55,30 +55,28 @@ window.onload = async (event) => {
                 method: 'POST',
                 body: formData
         
-            });//.then(response => response.text()).then(response =>{console.log(response)})
+            });
             let delete_result = await delete_response.json();
             alert(delete_result.status);
             window.location.reload();
         
-        
-          };
-       
+          }; 
     };
 
-    for (let i=0; i < edit_buttons.length; i++){
+    for (let i=0; i < edit_buttons.length; i++){  // when edit button is clicked
         edit_buttons[i].onclick= async (event) => {
             let post_id = edit_buttons[i].id;
             for (let i=0; i < posts.length; i++){
                 if (posts[i].id==post_id){
                    var post_edited = posts[i];
-                   post_edited.contentEditable = "true";
+                   post_edited.contentEditable = "true";  // post with the specific id will be editable
                     break; 
                 }
             }
 
 
-            edit_buttons[i].innerHTML="Save";
-            edit_buttons[i].onclick= async (event) => {
+            edit_buttons[i].innerHTML="Save"; // button edit will change to "Save" 
+            edit_buttons[i].onclick= async (event) => { // when button "Save" is clicked the API willbe called to the change the status and reload page
 
                 let formData = new FormData();
                 formData.append('post_id', post_id);
@@ -90,7 +88,7 @@ window.onload = async (event) => {
                     method: 'POST',
                     body: formData
             
-                });//.then(response => response.text()).then(response =>{console.log(response)})
+                });
                 let delete_result = await delete_response.json();
                 alert(delete_result.status);
                 window.location.reload();
@@ -100,60 +98,6 @@ window.onload = async (event) => {
 
             }
 
-
-         
-           /* let formData = new FormData();
-            formData.append('post_id', edit_buttons[i].id);
-        
-            event.preventDefault();
-        
-            let delete_response = await fetch(delete_url,{
-                method: 'POST',
-                body: formData
-        
-            });//.then(response => response.text()).then(response =>{console.log(response)})
-            let delete_result = await delete_response.json();
-            alert(delete_result.status);
-            window.location.reload();*/
-            console.log("test");
-        
-        
           };
-       
     };
-
 };
-
-
-
-
-/*async  (event) => {
-
-    let formData = new FormData();
-    formData.append('post_id', this.id);
-
-    event.preventDefault();
-
-    let delete_response = await fetch(delete_url,{
-        method: 'POST',
-        body: formData
-
-    });//.then(response => response.text()).then(response =>{console.log(response)})
-    let delete_result = await delete_response.json();
-    alert(delete_result.status)
-
-
-  };*/
-
-
- /*
-  function onClick(){
-
-    console.log("test")
-  }*/
-
-/*
-      for (let i=0; i < delete_buttons.length; i++){
-       
-    };
-      */
