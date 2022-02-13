dbdaefbd135ac8ad4bfca3f48481358da101f297
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-02-13 22:05:33 +0200

    completed sign in html and javascript

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 // Ali 
 
+let form_id = document.getElementById("user_form")
+
+try {
+form_id.onsubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch("http://localhost/fanfield%20project/fanfield_backend/signin_api.php", {
+        method: 'POST',
+        body: new FormData(user_form)
+    })
+
+    let result = await response.json();
+
+    if (result.status == "success"){
+        window.location.href="file:///C:/xampp/htdocs/fanfield%20project/fanfield_frontend/main_page.html";
+        localStorage.setItem('id',result.id);
+
+        let user_id = localStorage.getItem('id');             
+    }
+}
+}catch (error) {
+    console.log(error);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Zaraket
