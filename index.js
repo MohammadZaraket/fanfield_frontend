139ca1b340dbe1ca743c60279517f92540065a4b
AAAzzam1 <97559252+AAAzzam1@users.noreply.github.com>
2022-02-13 23:42:37 +0200

    completed sign up javascript

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // Ali 
 // sign in 
-let form_id = document.getElementById("sign_in_form")
+let sign_in_form_id = document.getElementById("sign_in_form")
 
 try {
-form_id.onsubmit = async (event) => {
+sign_in_form_id.onsubmit = async (event) => {
     event.preventDefault();
 
-    const response = await fetch("http://localhost/fanfield%20project/fanfield_backend/signin_api.php", {
+    const sign_in_response = await fetch("http://localhost/fanfield%20project/fanfield_backend/signin_api.php", {
         method: 'POST',
         body: new FormData(sign_in_form)
     })
 
-    let result = await response.json();
+    let sign_in_result = await sign_in_response.json();
 
-    if (result.status == "success"){
+    if (sign_in_result.status == "success"){
         window.location.href="file:///C:/xampp/htdocs/fanfield%20project/fanfield_frontend/main_page.html";
-        localStorage.setItem('id',result.id);
+        localStorage.setItem('id',sign_in_result.id);
 
         let user_id = localStorage.getItem('id');             
     }
@@ -25,11 +25,30 @@ form_id.onsubmit = async (event) => {
   }
 
 // sign up
-
 document.getElementById("sign_up_button").onclick = () => {
     document.getElementById("sign_in_box").style.display = "none"
     document.getElementById("before_sign_up").style.display = "contents"
 }
+
+let sign_up_form_id = document.getElementById("sign_up_form")
+
+try {
+sign_up_form_id.onsubmit = async (event) => {
+    event.preventDefault();
+    const sign_up_response = await fetch("http://localhost/fanfield%20project/fanfield_backend/signup_api.php", {
+        method: 'POST',
+        body: new FormData(sign_up_form)
+    })
+    console.log(sign_up_response)
+    let sign_up_result = await sign_up_response.json();
+ 
+    if (sign_up_result.status == "success"){
+        window.location.href="file:///C:/xampp/htdocs/fanfield%20project/fanfield_frontend/index.html";           
+    }
+}
+}catch (error) {
+    console.log(error);
+  }
 
 
 
