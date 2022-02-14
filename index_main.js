168d50d1e54018f69af8fc3053484fdd6d4619b0
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-14 17:46:07 +0200

    post a new status

--- index_main.js
+++ index_main.js
@@ -13,6 +13,7 @@ let formData = new FormData();
 formData.append('user_id', 1);
 
 let posts_url = new URL('http://localhost/Fanfield/fanfield_backend/view_status_api.php');
+let new_post_url = new URL('http://localhost/Fanfield/fanfield_backend/post_api.php');
 
 window.onload = async (event) => {
     event.preventDefault();
@@ -42,7 +43,20 @@ window.onload = async (event) => {
 post_form.onsubmit = async (event) => {
     event.preventDefault();
 
-    let post_input = document.getElementById("input").value;
+    let post_space = document.getElementById("post_space").value;
+
+    let formPost = new FormData();
+    formPost.append('user_id', 1);
+    formPost.append('status', post_space);
+
+    let post_response = await fetch(new_post_url,{
+        method: 'POST',
+        body: formPost
+
+    });
+    let post_result = await post_response.json();
+    alert(post_result.status);
+    window.location.reload();
    
  
 };
