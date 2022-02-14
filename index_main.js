70e8534cc365e3d2c6c3375f4b5de79baae5b114
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-14 16:13:38 +0200

    design like button

--- index_main.js
+++ index_main.js
@@ -5,6 +5,9 @@
 let post_div = document.getElementById("posts_views");
 
 
+let post_btn = document.getElementById("post_btn");
+let post_form = document.getElementById("post_form");
+
 
 let formData = new FormData();
 formData.append('user_id', 1);
@@ -24,7 +27,7 @@ window.onload = async (event) => {
     for (let i=0; i < post_result.length; i++){
 
         post_div.innerHTML+="<label class='label'>" +post_result[i].first_name +" "+post_result[i].last_name + "</label><div class='row'><div class='col-9'><h3 class='post_feed'>" + post_result[i].Post + "</h3></div>" +
-        "<div class='col-3'><button class='like' id="+post_result[i].Post_id+"> Like   " + "<i class='fas fa-edit'></i></button></div></div><hr>" ;
+        "<div class='col-3'><button class='like' id="+post_result[i].Post_id+">"+post_result[i].Number_of_Likes+"                   " + "<i class='fa fa-thumbs-o-up'></i></button></div></div><hr>" ;
         }
 
    /* var posts = document.getElementsByClassName("post");
@@ -32,3 +35,14 @@ window.onload = async (event) => {
     var delete_buttons = document.getElementsByClassName("delete");
     */
 }
+
+
+
+
+post_form.onsubmit = async (event) => {
+    event.preventDefault();
+
+    let post_input = document.getElementById("input").value;
+   
+ 
+};
