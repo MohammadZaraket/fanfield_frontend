d574dcd108296c144468e5ac198587a4aab86331
MohammadZaraket <mohammadmzaraket@gmail.com>
2022-02-14 14:28:06 +0200

    changing query in view status

---
+++ index_main.js
@@ -0,0 +1,34 @@
+// Zaraket
+
+
+
+let post_div = document.getElementById("posts_views");
+
+
+
+let formData = new FormData();
+formData.append('user_id', 1);
+
+let posts_url = new URL('http://localhost/Fanfield/fanfield_backend/view_status_api.php');
+
+window.onload = async (event) => {
+    event.preventDefault();
+
+    let post_response = await fetch(posts_url,{
+        method: 'POST',
+        body: formData
+
+    });//.then(response => response.text()).then(response =>{console.log(response)})
+    let post_result = await post_response.json();
+
+    for (let i=0; i < post_result.length; i++){
+
+        post_div.innerHTML+= "<label class='label'>" +post_result[i].first_name +" "+post_result[i].last_name + "</label>"+
+        "<h3 class='post_feed'>" + post_result[i].Post + "</h3> <hr>" ;
+        }
+
+   /* var posts = document.getElementsByClassName("post");
+    var edit_buttons = document.getElementsByClassName("edit");
+    var delete_buttons = document.getElementsByClassName("delete");
+    */
+}
